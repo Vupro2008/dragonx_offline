99611e31d3cb9b3baa4ac7f6fd77d486ff876598
Vupro2008 <104565448+Vupro2008@users.noreply.github.com>
2023-08-27 11:09:24 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+curl -o /data/data/com.termux/files/usr/dragonx_offline/_v3.3_dragonx_.zip https://github.com/Vupro2008/dragonx_offline/raw/main/_v3.3_dragonx_.zip
+
+cd /data/data/com.termux/files/usr/dragonx_offline/ && unzip -o _v3.3_dragonx_.zip
+
+rm -f /data/data/com.termux/files/usr/dragonx_offline/_v3.3_dragonx_.zip
+echo
+echo
+echo
+echo
+echo "\033[1;32m SETUP THÀNH CÔNG!!!"
