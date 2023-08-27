5314c748c3cfe6cd748e515f851f751d1ce20075
Vupro2008 <104565448+Vupro2008@users.noreply.github.com>
2023-08-27 11:14:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -o /data/data/com.termux/files/usr/dragonx_offline/_v3.3_dragonx_.zip https://github.com/Vupro2008/dragonx_offline/raw/main/_v3.3_dragonx_.zip
+curl -o /data/data/com.termux/files/usr/dragonx_offline/_v3.3_dragonx_.zip https://raw.githubusercontent.com/Vupro2008/dragonx_offline/main/_v3.3_dragonx_.zip
 
 cd /data/data/com.termux/files/usr/dragonx_offline/ && unzip -o _v3.3_dragonx_.zip
 
